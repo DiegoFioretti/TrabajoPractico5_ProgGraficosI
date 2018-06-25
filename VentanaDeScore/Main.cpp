52b5eb87088dcf89296e0701babde6d110740c47
DiegoFioretti <diego_fioretti_97@hotmail.com>
2018-06-25 17:11:37 -0300

    Codigo base
    Escrito el codigo necesario para la aparicion de una ventana con los 5 highscores presentes al ejecutar

--- VentanaDeScore/Main.cpp
+++ VentanaDeScore/Main.cpp
@@ -1,6 +1,5 @@
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/label.hpp>
-#include <nana/gui/widgets/button.hpp>
 #include <ScoreManager.h>
 #include <iostream>
 using namespace std;
@@ -24,22 +23,36 @@ int main(){
 	using namespace nana;
 	
 	//Define a form.
-	form fm;
+	form fm(API::make_center(200, 500));
 
 	//Define a label and display a text.
-	label lab{ fm, "<bold size=16> Scores" };
-	lab.format(true);
+	label title{ fm, "<bold size=16> Scores" };
+	title.format(true);
 
-	//Define a button and answer the click event.
-	/*button btn{ fm, "Quit" };
-	btn.events().click([&fm] {
-		fm.close(); // ACA VA EL CODIGO QUE QUEREMOS CUANDO SE PRESIONA EL BOTÓN
-	});*/
+	label test1{ fm, "<size = 10> FIRST" };
+	test1.format(true);
+
+	label test2{ fm, "<size = 10> SECOND" };
+	test2.format(true);
+
+	label test3{ fm, "<size = 10> THIRD" };
+	test3.format(true);
+
+	label test4{ fm, "<size = 10> FOURTH" };
+	test4.format(true);
+
+	label test5{ fm, "<size = 10> FIFTH" };
+	test5.format(true);
 
 	//Layout management
-	fm.div("vert <<><><<><weight=180% text><>><><<><>><>");
-	fm["text"] << lab;
-	//fm["button"] << btn;
+
+	fm.div("vert <<><text weight=100><>> <> <<><text1 weight=80><>> <> <<><text2 weight=80><>> <> <<><text3 weight=80><>> <> <<><text4 weight=80><>> <> <<><text5 weight=80><>>");
+	fm["text"] << title;
+	fm["text1"] << test1;
+	fm["text2"] << test2;
+	fm["text3"] << test3;
+	fm["text4"] << test4;
+	fm["text5"] << test5;
 	fm.collocate();
 
 	//Show the form
