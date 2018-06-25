94dd8eba56fb26e2143e71a9b641a8881368c8d6
DiegoFioretti <diego_fioretti_97@hotmail.com>
2018-06-25 19:09:59 -0300

    Implementacion codigo de Score
    +Creada una lista de scores y el codigo base para su correcto funcionamiento.
    +Añadido el analisis de la libreria de Scores.

--- VentanaDeScore/Main.cpp
+++ VentanaDeScore/Main.cpp
@@ -4,21 +4,27 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	ScoreManager scoreList;
+// Lista de Scores
+ScoreManager scoreList;
 
-	scoreList.addScore(190);
-	scoreList.updateHighScore();
-	scoreList.addScore(300);
-	scoreList.updateHighScore();
-	scoreList.addScore(200);
-	scoreList.updateHighScore();
-	scoreList.addScore(50);
-	scoreList.updateHighScore();
-	scoreList.addScore(250);
+void scoreSimulation() {
+	scoreList.setScore(190);
+	scoreList.finishScore();
+	scoreList.setScore(300);
+	scoreList.finishScore();
+	scoreList.setScore(200);
+	scoreList.finishScore();
+	scoreList.setScore(50);
+	scoreList.finishScore();
+	scoreList.setScore(250);
+	scoreList.finishScore();
+
 	scoreList.updateHighScore();
 
 	scoreList.getAll();
+}
+
+int main(){
 
 	using namespace nana;
 	
@@ -29,24 +35,24 @@ int main(){
 	label title{ fm, "<bold size=16> Scores" };
 	title.format(true);
 
-	label test1{ fm, "<size = 10> FIRST" };
+	label test1{ fm, "<size = 10> FIRST: " };
 	test1.format(true);
 
-	label test2{ fm, "<size = 10> SECOND" };
+	label test2{ fm, "<size = 10> SECOND: " };
 	test2.format(true);
 
-	label test3{ fm, "<size = 10> THIRD" };
+	label test3{ fm, "<size = 10> THIRD: " };
 	test3.format(true);
 
-	label test4{ fm, "<size = 10> FOURTH" };
+	label test4{ fm, "<size = 10> FOURTH: " };
 	test4.format(true);
 
-	label test5{ fm, "<size = 10> FIFTH" };
+	label test5{ fm, "<size = 10> FIFTH: " };
 	test5.format(true);
 
 	//Layout management
 
-	fm.div("vert <<><text weight=100><>> <> <<><text1 weight=80><>> <> <<><text2 weight=80><>> <> <<><text3 weight=80><>> <> <<><text4 weight=80><>> <> <<><text5 weight=80><>>");
+	fm.div("vert <<><text weight=100><>> <> <<><text1 weight=100><>> <> <<><text2 weight=140><>> <> <<><text3 weight=100><>> <> <<><text4 weight=120><>> <> <<><text5 weight=90><>>");
 	fm["text"] << title;
 	fm["text1"] << test1;
 	fm["text2"] << test2;
