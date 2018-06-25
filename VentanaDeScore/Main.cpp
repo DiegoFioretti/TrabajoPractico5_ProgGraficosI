fac5c49dc45285f490acb3c552c693dc51a3df03
DiegoFioretti <diego_fioretti_97@hotmail.com>
2018-06-25 20:31:33 -0300

    Version 1.0
    +Existe una funcion de pruebas que carga la lista de Score con 5 numeros.
    +La funcion que muestra los Scores muestra los primeros 5 valores, puede ser modificado para mostrar mas si es necesario.

--- VentanaDeScore/Main.cpp
+++ VentanaDeScore/Main.cpp
@@ -1,22 +1,27 @@
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/label.hpp>
 #include <ScoreManager.h>
+#include <string>
 #include <iostream>
+using namespace nana;
 using namespace std;
 
 // Lista de Scores
 ScoreManager scoreList;
 
+//Forma ventana
+form fm(API::make_center(200, 500));
+
 void scoreSimulation() {
-	scoreList.setScore(190);
+	scoreList.setScore(30);
 	scoreList.finishScore();
 	scoreList.setScore(300);
 	scoreList.finishScore();
-	scoreList.setScore(200);
+	scoreList.setScore(430);
 	scoreList.finishScore();
 	scoreList.setScore(50);
 	scoreList.finishScore();
-	scoreList.setScore(250);
+	scoreList.setScore(252);
 	scoreList.finishScore();
 
 	scoreList.updateHighScore();
@@ -24,34 +29,29 @@ void scoreSimulation() {
 	scoreList.getAll();
 }
 
-int main(){
-
-	using namespace nana;
-	
-	//Define a form.
-	form fm(API::make_center(200, 500));
-
-	//Define a label and display a text.
+void scoreScreen() {
+	//Definir texto a mostrar
 	label title{ fm, "<bold size=16> Scores" };
 	title.format(true);
 
-	label test1{ fm, "<size = 10> FIRST: " };
+	string score = to_string(scoreList.getScore(0));
+	label test1{ fm, "<size = 10> FIRST: " + score };
 	test1.format(true);
-
-	label test2{ fm, "<size = 10> SECOND: " };
+	score = to_string(scoreList.getScore(1));
+	label test2{ fm, "<size = 10> SECOND: " + score };
 	test2.format(true);
-
-	label test3{ fm, "<size = 10> THIRD: " };
+	score = to_string(scoreList.getScore(2));
+	label test3{ fm, "<size = 10> THIRD: " + score };
 	test3.format(true);
-
-	label test4{ fm, "<size = 10> FOURTH: " };
+	score = to_string(scoreList.getScore(3));
+	label test4{ fm, "<size = 10> FOURTH: " + score };
 	test4.format(true);
-
-	label test5{ fm, "<size = 10> FIFTH: " };
+	score = to_string(scoreList.getScore(4));
+	label test5{ fm, "<size = 10> FIFTH: " + score };
 	test5.format(true);
 
-	//Layout management
 
+	//Mostrar 
 	fm.div("vert <<><text weight=100><>> <> <<><text1 weight=100><>> <> <<><text2 weight=140><>> <> <<><text3 weight=100><>> <> <<><text4 weight=120><>> <> <<><text5 weight=90><>>");
 	fm["text"] << title;
 	fm["text1"] << test1;
@@ -61,9 +61,13 @@ int main(){
 	fm["text5"] << test5;
 	fm.collocate();
 
-	//Show the form
 	fm.show();
 
-	//Start to event loop process, it blocks until the form is closed.
 	exec();
+}
+
+int main(){
+	scoreSimulation();
+
+	scoreScreen();
 }
